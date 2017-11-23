176a542243c4addbe7346a438f45bb85da0bfca0
zondahuman <zondahuman@gmail.com>
2017-11-23 18:41:42 +0800

    add

---
+++ algorithm-basic/src/test/java/com/abin/lee/algorithm/basis/test/BasisTest.java
@@ -0,0 +1,18 @@
+package com.abin.lee.algorithm.basis.test;
+
+import com.google.common.base.Strings;
+
+/**
+ * Created by abin on 2017/11/23 18:40.
+ * algorithm-svr
+ * com.abin.lee.algorithm.basis.test
+ */
+public class BasisTest {
+
+    public static void main(String[] args) {
+        String param = " ";
+        boolean flag = Strings.isNullOrEmpty(param);
+        System.out.println("flag= "+flag);
+    }
+
+}
