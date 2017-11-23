8893c74190a9cbeaeab593fd3b41168f3e3116a4
zondahuman <zondahuman@gmail.com>
2017-11-23 19:47:01 +0800

    add

--- algorithm-basic/src/test/java/com/abin/lee/algorithm/basis/test/BasisTest.java
+++ algorithm-basic/src/test/java/com/abin/lee/algorithm/basis/test/BasisTest.java
@@ -1,6 +1,13 @@
 package com.abin.lee.algorithm.basis.test;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Created by abin on 2017/11/23 18:40.
@@ -13,6 +20,28 @@ public class BasisTest {
         String param = " ";
         boolean flag = Strings.isNullOrEmpty(param);
         System.out.println("flag= "+flag);
+
+        Map<String, Integer> frequence = Maps.newHashMap();
+        frequence.put("abin10", 5);
+        frequence.put("abin6", 2);
+        frequence.put("abin3", 8);
+        frequence.put("abin8", 7);
+        Map<String, Integer> result = sortByValue(frequence);
+        for(Iterator<Map.Entry<String, Integer>> iterator = result.entrySet().iterator(); iterator.hasNext();){
+            Map.Entry<String, Integer> entry = iterator.next();
+            System.out.println("key="+entry.getKey() + ", value="+entry.getValue());
+        }
     }
+
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        Map<K, V> result = new LinkedHashMap<>();
+        Stream<Map.Entry<K, V>> st = map.entrySet().stream();
+
+        st.sorted(Comparator.comparing(e -> e.getValue())).forEach(e -> result.put(e.getKey(), e.getValue()));
+
+        return result;
+    }
+
 
 }
