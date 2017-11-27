6612ce5e6fd6caa2e68404bd7e2d0e5ba29352c2
zondahuman <zondahuman@gmail.com>
2017-11-27 14:22:26 +0800

    add

--- algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/BinaryTreeMirror.java
+++ algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/BinaryTreeMirror.java
@@ -5,6 +5,8 @@ import com.qunar.des.algorithm.common.json.jackson.JsonUtil;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Stack;
+
 /**
  * Created by abin on 2017/11/27 13:38.
  * algorithm-svr
@@ -29,15 +31,60 @@ import lombok.Setter;
  交换根节点的左右孩子，再分别对左孩子进行镜像和右孩子进行镜像
  */
 public class BinaryTreeMirror {
-    public static MirrorNode mirrorRecurive(MirrorNode node){
+    public static MirrorNode mirrorRecursive(MirrorNode node){
         if(null == node)
             return null;
         MirrorNode temp = node.left;
         node.left = node.right;
         node.right = temp;
-        mirrorRecurive(node.left);
-        mirrorRecurive(node.right);
+        mirrorRecursive(node.left);
+        mirrorRecursive(node.right);
         return node;
+    }
+
+    public static Stack<MirrorNode> mirrorLoop(MirrorNode root){
+        if(null == root)
+            return null;
+        Stack<MirrorNode> stack = new Stack<>();//借助于辅助栈
+        MirrorNode current = null;//存放出栈的栈顶元素
+        MirrorNode temp = null;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            current = stack.pop(); //将根元素出栈 交换根元素的左右子树
+            if(current.left != null || current.right != null){
+                temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+            }
+            //将根元素的左右孩子压入栈中
+            if(current.left != null)
+                stack.push(current.left);
+            if(current.right != null)
+                stack.push(current.right);
+        }
+        return stack;
+    }
+
+    public static void mirrorCycle(MirrorNode root){
+        if(null == root)
+            return;
+        Stack<MirrorNode> stack = new Stack<>();//借助于辅助栈
+        MirrorNode current = null;//存放出栈的栈顶元素
+        MirrorNode temp = null;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            current = stack.pop(); //将根元素出栈 交换根元素的左右子树
+            if(current.left != null || current.right != null){
+                temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+            }
+            //将根元素的左右孩子压入栈中
+            if(current.left != null)
+                stack.push(current.left);
+            if(current.right != null)
+                stack.push(current.right);
+        }
     }
 
     public static void main(String[] args) {
@@ -49,8 +96,15 @@ public class BinaryTreeMirror {
         MirrorNode node6 = new MirrorNode(6, node4, node5);
         MirrorNode node7 = new MirrorNode(7, node6, node3);
         System.out.println("mirrorNode1---before = " + JsonUtil.toJson(node7));
-        MirrorNode mirrorNode1 = mirrorRecurive(node7);
-        System.out.println("mirrorNode1---after = " + JsonUtil.toJson(mirrorNode1));
+//        MirrorNode mirrorNode1 = mirrorRecursive(node7);
+//        System.out.println("mirrorNode1---after = " + JsonUtil.toJson(mirrorNode1));
+
+        mirrorLoop(node7);
+        System.out.println("mirrorNode2---after = " + JsonUtil.toJson(node7));
+
+//        mirrorCycle(node7);
+//        System.out.println("mirrorNode3---after = " + JsonUtil.toJson(node7));
+
 
     }
 }
