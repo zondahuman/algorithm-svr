a8480a824a2d96b2af967e94e868a5e8b29c30e9
zondahuman <zondahuman@gmail.com>
2017-12-07 22:23:21 +0800

    add

--- algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/BinaryTreeAddNext.java
+++ algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/BinaryTreeAddNext.java
@@ -17,6 +17,20 @@ public class BinaryTreeAddNext {
     /**
      * 二叉树系列---标记二叉树中每个节点的next指针
      * http://blog.csdn.net/bh_xiaoxinba/article/details/53030689
+     * For example,
+     Given the following perfect binary tree,
+     1
+     / \
+     2 3
+     / \ / \
+     4 5 6 7
+
+     After calling your function, the tree should look like:
+     1 -> NULL
+     / \
+     2 -> 3 -> NULL
+     / \ / \
+     4->5->6->7 -> NULL
      * @param args
      */
     public static void main(String[] args) {
