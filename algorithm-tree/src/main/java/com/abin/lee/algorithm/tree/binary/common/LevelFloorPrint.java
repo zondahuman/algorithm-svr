29473ddb792c9ca3bcb49b8f0381f14f43aaf19a
lee <lisubin@we.com>
2018-03-13 14:28:15 +0800

    add

--- algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/LevelFloorPrint.java
+++ algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/LevelFloorPrint.java
@@ -13,6 +13,13 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class LevelFloorPrint {
 
     public static void main(String[] args) throws InterruptedException {
+
+//        new LevelFloorPrint().printFloorCommon();
+        new LevelFloorPrint().printFloorN();
+
+    }
+
+    public void printFloorCommon() throws InterruptedException {
         TreeNode t1 = new TreeNode(1);
         TreeNode t2 = new TreeNode(2);
         TreeNode t3 = new TreeNode(3);
@@ -30,8 +37,42 @@ public class LevelFloorPrint {
         t5.left = t7;
         t5.right = t8;
 //        new LevelFloorPrint().printFloor(t1);
-        new LevelFloorPrint().printFloors(t1);
+        this.printFloors(t1);
+    }
 
+    public void printFloorN() throws InterruptedException {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
+        TreeNode t8 = new TreeNode(8);
+        TreeNode t9 = new TreeNode(9);
+        TreeNode t10 = new TreeNode(10);
+        TreeNode t11 = new TreeNode(11);
+        TreeNode t12 = new TreeNode(12);
+        TreeNode t13 = new TreeNode(13);
+        TreeNode t14 = new TreeNode(14);
+        TreeNode t15 = new TreeNode(15);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t9;
+        t3.left = t5;
+        t3.right = t6;
+        t4.left = t10;
+        t4.right = t11;
+        t5.left = t7;
+        t5.right = t8;
+        t6.left = t12;
+        t6.right = t13;
+        t7.left = t14;
+        t7.right = t15;
+
+        this.printFloors(t1);
     }
 
     public void printFloor(TreeNode root) throws InterruptedException {
