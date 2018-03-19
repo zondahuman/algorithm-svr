8a28eda721d8ccf45670aaec5c3ec5f6c4343898
zondahuman <zondahuman@gmail.com>
2018-03-19 18:10:57 +0800

    ad

--- algorithm-basic/src/main/java/com/qunar/des/algorithm/basic/lru/LRULinkedHashMap.java
+++ algorithm-basic/src/main/java/com/qunar/des/algorithm/basic/lru/LRULinkedHashMap.java
@@ -7,8 +7,9 @@ import java.util.Map;
  * Created with IntelliJ IDEA.
  * User: abin
  * Date: 15-4-23 下午3:20
+ * http://blog.csdn.net/wangxilong1991/article/details/70172302
  */
-public class LRULinkedHashMap {
+public class LRULinkedHashMap<K, V>  {
     public static void main(String[] args) {
         Map<String,String> request = new LinkedHashMap<String, String>();
         for(int i=0;i<100;i++){
@@ -16,4 +17,7 @@ public class LRULinkedHashMap {
         }
 //        request.
     }
+
+
+    
 }
