c25b6cf0a3cd92d12c40fda10c0f79a0e5c69598
zondahuman <zondahuman@gmail.com>
2018-04-27 20:05:11 +0800

    add

--- algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/string/common/MaxCommonString.java
+++ algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/string/common/MaxCommonString.java
@@ -1,15 +1,18 @@
 package com.abin.lee.algorithm.basic.string.common;
 
 /**
- * java 温习之字符串（获取两个字符串的最大公共字串）
+ * 获取两个字符串的最大公共子串
+ * 找出两个字符串的最大公共子串
+ * https://blog.csdn.net/cleverlzc/article/details/49363541
  */
 public class MaxCommonString {
     public static void main(String[] args) {
-        String str1 = "adcfgeheightkdeffcser";
-        String str2 = "theightyye";
-
+//        String str1 = "adcfgeheightkdeffcser";
+//        String str2 = "theightyye";
+        String str1 = "abcdefg";
+        String str2 = "abdefg";
         String result = getMaxString(str1, str2);
-        System.out.println(result);
+        System.out.println("result=" + result);
     }
 
     private static String getMaxString(String str1, String str2) {
@@ -18,7 +21,9 @@ public class MaxCommonString {
         max = (str1.length() > str2.length() ? str1 : str2);
         min = max.equals(str1) ? str2 : str1;
         for (int i = 0; i < min.length(); i++) {
-            for (int start = 0, end = min.length() - i; end != min.length() + 1; start++, end++) {
+            // 遍历较短的字符串，并依次减少短字符串的字符数量，判断长字符是否包含该子串
+            for (int start = 0, end = min.length() - i; end <= min.length(); start++, end++) {
+//            for (int start = 0, end = min.length() - i; end != min.length() + 1; start++, end++) {
                 String sub = min.substring(start, end);
                 if (max.contains(sub))
                     return sub;
@@ -26,5 +31,8 @@ public class MaxCommonString {
         }
         return null;
     }
+
+
+
 
 }
