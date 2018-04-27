22afd023197b54e4dc1e8d6570df447baee59d13
zondahuman <zondahuman@gmail.com>
2018-04-27 21:59:05 +0800

    add

--- algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/string/common/FindMaxSequenceString.java
+++ algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/string/common/FindMaxSequenceString.java
@@ -5,6 +5,7 @@ package com.abin.lee.algorithm.basic.string.common;
  * 找出两个字符串的最大公共子序列
  * https://www.ojcoder.cn/LintCode/79-longest-common-substring/index.php?slang=java
  *
+ *
  */
 public class FindMaxSequenceString {
     public static void main(String[] args) {
@@ -14,6 +15,8 @@ public class FindMaxSequenceString {
         String str2 = "abdefg";
         int result = longestCommonSubstring(str1, str2);
         System.out.println("result=" + result);
+        int result2 = lcs(str1, str2);
+        System.out.println("result2=" + result2);
     }
 
     /**
@@ -38,6 +41,28 @@ public class FindMaxSequenceString {
         return maxLen;
     }
 
+
+    /**
+     *  1、 lcs(i,j)=0      (i=0, j=0)
+     *  2、 lcs(i,j)=lcs(i-1,j-1)+1  (i=j )
+     *  3、 lcs(i,j)= max(lcs(i,j-1), lcs(i-1,j))  (i!=j )
+     * @param param1
+     * @param param2
+     * @return
+     */
+    public static int lcs(String param1, String param2){
+        int[][] result = new int[param1.length()+1][param2.length()+1];
+        for (int i = 1; i <=param1.length() ; i++) {
+            for (int j = 1; j <=param2.length() ; j++) {
+                if(param1.charAt(i-1) == param2.charAt(j-1)){
+                    result[i][j] = result[i-1][j-1] + 1;
+                }else{
+                    result[i][j] = Math.max(result[i-1][j], result[i][j-1]);
+                }
+            }
+        }
+        return result[param1.length()][param2.length()];
+    }
 
 
 
