3acd8d06bd75a4888a2d6cb4ad44de5d6ac09d3f
zondahuman <zondahuman@gmail.com>
2018-06-25 00:34:07 +0800

    add

---
+++ algorithm-basic/src/test/java/com/abin/lee/algorithm/basic/strings/test/StringtoIntegerTest.java
@@ -0,0 +1,28 @@
+package com.abin.lee.algorithm.basic.strings.test;
+
+/**
+ * Created by abin on 2018/6/25.
+ */
+public class StringtoIntegerTest {
+    public static void main(String[] args) {
+        String param = "1234" ;
+        Integer result = transfer(param);
+        System.out.println("result="+result);
+    }
+
+    public static Integer transfer(String str){
+        int sign = 1 ;
+        int index = 0;
+        if(str.charAt(index) == '-') {
+            index++;
+            sign = -1;
+        }
+        int num = 0;
+        for (;index<str.length();index++){
+            num = num*10 + (str.charAt(index)-'0');
+        }
+        return sign*num;
+    }
+
+
+}
