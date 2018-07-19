09339d8180c28ea5ee4cfa519174002a4d799936
zondahuman <zondahuman@gmail.com>
2018-07-19 20:57:47 +0800

    add

--- algorithm-leetcode/src/main/java/com/qunar/des/algorithm/nowcoder/BinaryTreeDepth.java
+++ algorithm-leetcode/src/main/java/com/qunar/des/algorithm/nowcoder/BinaryTreeDepth.java
@@ -5,6 +5,8 @@ package com.qunar.des.algorithm.nowcoder;
  * 二叉树的深度
  题目描述
  输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ 104. Maximum Depth of Binary Tree
+ https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
  */
 public class BinaryTreeDepth {
     public int TreeDepth(TreeNode root) {
@@ -13,7 +15,7 @@ public class BinaryTreeDepth {
         return Math.max(TreeDepth(root.left), TreeDepth(root.right)) +1;
     }
 
-    
+
 
 
     public static class TreeNode {
---
+++ algorithm-leetcode/src/main/java/com/qunar/des/algorithm/nowcoder/BinaryTreeIsBalanced.java
@@ -0,0 +1,51 @@
+package com.qunar.des.algorithm.nowcoder;
+
+/**
+ * Created by abin on 2018/7/19.
+ */
+public class BinaryTreeIsBalanced {
+
+    /**
+     * nowcoder
+     * @param root
+     * @return
+     */
+    public boolean IsBalanced_Solution(TreeNode root) {
+        if(root == null)
+            return true;
+        if(Math.abs(maxDepth(root.left) - maxDepth(root.right))>1)
+            return false;
+        return IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right) ;
+    }
+
+    /**
+     * leecode
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+            return true;
+        if(Math.abs(maxDepth(root.left) - maxDepth(root.right))>1)
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right) ;
+    }
+
+    public int maxDepth(TreeNode root){
+        if(root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+}
