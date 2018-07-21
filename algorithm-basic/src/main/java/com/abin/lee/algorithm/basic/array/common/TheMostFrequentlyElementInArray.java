8c5e0bea8e63857925a280f872896decc2c5cedf
zondahuman <zondahuman@gmail.com>
2018-07-21 23:04:32 +0800

    add

--- algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/array/common/TheMostFrequentlyElementInArray.java
+++ algorithm-basic/src/main/java/com/abin/lee/algorithm/basic/array/common/TheMostFrequentlyElementInArray.java
@@ -1,6 +1,7 @@
 package com.abin.lee.algorithm.basic.array.common;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -8,6 +9,8 @@ import java.util.Map;
  * 数组中出现次数最多的元素
  * 数组中重复次数最多的元素
  * https://blog.csdn.net/u014473112/article/details/54176803
+ * 169. Majority Element
+ * https://leetcode.com/problems/majority-element/description/
  */
 public class TheMostFrequentlyElementInArray {
 
@@ -33,6 +36,49 @@ public class TheMostFrequentlyElementInArray {
             if (map.get(params[i]) > max) {
                 max = map.get(params[i]);
                 index = params[i];
+            }
+        }
+        return index;
+    }
+
+    public int majorityElement(int[] nums) {
+        int num = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                int temp = map.get(nums[i]);
+                map.put(nums[i], ++temp);
+            }else{
+                map.put(nums[i], 1);
+            }
+        }
+        int temp = 0;
+        for (Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            int count = entry.getValue() ;
+            if(count > temp) {
+                temp = count;
+                num = entry.getKey();
+            }
+        }
+        return num;
+    }
+
+    public int majorityElement1(int[] nums) {
+        int max = 0;
+        int index = 0;
+        if (nums == null || nums.length == 0)
+            return 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+            if (map.get(nums[i]) > max) {
+                max = map.get(nums[i]);
+                index = nums[i];
             }
         }
         return index;
