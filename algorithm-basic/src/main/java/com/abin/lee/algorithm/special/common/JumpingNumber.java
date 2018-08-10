70a3c872e256a9d06ad4f10569fb9de63000f11d
zondahuman <zondahuman@gmail.com>
2018-08-10 22:48:36 +0800

    add

---
+++ algorithm-basic/src/main/java/com/abin/lee/algorithm/special/common/JumpingNumber.java
@@ -0,0 +1,30 @@
+package com.abin.lee.algorithm.special.common;
+
+/**
+ * Created by abin on 2018/8/10.
+ * 跳跃的数字2（动态规划实现)
+ * https://blog.csdn.net/tugangkai/article/details/72842321
+ */
+public class JumpingNumber {
+    /**
+     * 给定一个非负整数数组，假定你的初始位置为数组第一个下标。
+     数组中的每个元素代表你在那个位置能够跳跃的最大长度。
+     你的目标是到达最后一个下标，并且使用最少的跳跃次数。
+     例如：
+     A = [2,3,1,1,4]A=[2,3,1,1,4]，到达最后一个下标的最少跳跃次数为 22。（先跳跃 11 步，从下标 00 到 11，然后跳跃 33 步，到达最后一个下标。一共两次）
+     输入格式
+     第一行输入一个正整数 n(1 \le n \le 100)n(1≤n≤100) ，接下来的一行，输入 nn 个整数，表示数组 AA。
+     输出格式
+     最后输出最少的跳跃次数
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int jumpNumber(int[] input){
+
+        return 0 ;
+    }
+
+}
---
+++ algorithm-tree/src/main/java/com/abin/lee/algorithm/tree/binary/common/BinaryTreeSumNAllPath.java
@@ -0,0 +1,63 @@
+package com.abin.lee.algorithm.tree.binary.common;
+
+import com.qunar.des.algorithm.common.json.jackson.JsonUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Stack;
+
+/**
+ * Created by abin on 2018/8/10.
+ * 找出所有从根节点到叶子节点路径和等于n的路径并输出
+ * 一个二叉树，要求从二叉树根节点一直到叶子节点数字之和等于n，给出符合的路径的数量
+ * 一个二叉树，每个节点都是数字，给定一个数字n，要求从二叉树根节点一直到叶子节点数字之和等于n，给出符合的路径的数量
+ * https://blog.csdn.net/yan456jie/article/details/52016685
+ * https://blog.csdn.net/yan456jie/article/details/50967459
+ */
+public class BinaryTreeSumNAllPath {
+    Stack<Node> stack = new Stack<Node>();
+    public void findPath(Node root, int k){
+        if(root != null){
+            stack.push(root);
+            k = k - root.value ;
+            if(k==0 && root.left==null && root.right==null){
+                for (Node no:stack){
+                    System.out.print(no.value+"-");
+                }
+                System.out.println();
+            }
+            findPath(root.left, k);
+            findPath(root.right, k);
+            stack.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(3);
+        root.left.right = new Node(2);
+        root.right.left = new Node(2);
+        root.right.right = new Node(4);
+        System.out.println("root="+ JsonUtil.toJson(root));
+        new BinaryTreeSumNAllPath().findPath(root, 6);
+
+    }
+
+    @Setter
+    @Getter
+    public static class Node{
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+}
