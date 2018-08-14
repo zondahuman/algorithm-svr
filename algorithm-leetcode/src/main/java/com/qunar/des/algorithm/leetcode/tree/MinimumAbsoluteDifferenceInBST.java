0e6de08309fb57caa8d709180dfc00544795f0ee
zondahuman <zondahuman@gmail.com>
2018-08-14 23:02:28 +0800

    add

---
+++ algorithm-leetcode/src/main/java/com/qunar/des/algorithm/leetcode/tree/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,57 @@
+package com.qunar.des.algorithm.leetcode.tree;
+
+import com.qunar.des.algorithm.common.json.jackson.JsonUtil;
+
+/**
+ * Created by abin on 2018/8/14.
+ * 530. Minimum Absolute Difference in BST
+ * https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+ * https://blog.csdn.net/tstsugeg/article/details/59110141
+ * https://blog.csdn.net/mcf171/article/details/57412179
+ */
+public class MinimumAbsoluteDifferenceInBST {
+    int min = Integer.MAX_VALUE;
+    TreeNode previous = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return min;
+    }
+
+    public void inorder(TreeNode root) {
+        if (null == root)
+            return;
+        inorder(root.left);
+        if (null != previous)
+            min = Math.min(min, root.val - previous.val);
+        previous = root;
+        inorder(root.right);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+//        root.left.left = new TreeNode(3);
+//        root.left.right = new TreeNode(4);
+//        root.right.left = new TreeNode(13);
+//        root.right.right = new TreeNode(14);
+        System.out.println("root=" + JsonUtil.toJson(root));
+
+        int min = new MinimumAbsoluteDifferenceInBST().getMinimumDifference(root);
+        System.out.println("min=" + JsonUtil.toJson(min));
+    }
+
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+}
