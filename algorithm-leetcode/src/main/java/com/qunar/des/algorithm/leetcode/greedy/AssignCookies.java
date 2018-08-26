4536e105d49658793bb5cee760b58d8bd11ab57b
zondahuman <zondahuman@gmail.com>
2018-08-26 16:58:20 +0800

    add

---
+++ algorithm-leetcode/src/main/java/com/qunar/des/algorithm/leetcode/greedy/AssignCookies.java
@@ -0,0 +1,22 @@
+package com.qunar.des.algorithm.leetcode.greedy;
+
+/**
+ * Created by abin on 2018/8/26.
+ * 455. Assign Cookies
+ https://leetcode.com/problems/assign-cookies/description/
+ */
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] param = new int[] {0,1,0};
+//        int[] param = new int[] {0,2,1,0};
+        int result = new AssignCookies().findContentChildren(param);
+        System.out.println("result=" + result);
+    }
+
+
+}
