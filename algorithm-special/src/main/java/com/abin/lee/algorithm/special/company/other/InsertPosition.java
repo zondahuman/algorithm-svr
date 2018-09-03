493694a381e074b50391d1937c22671921ae7f88
zondahuman <zondahuman@gmail.com>
2018-09-03 17:40:59 +0800

    add

---
+++ algorithm-special/src/main/java/com/abin/lee/algorithm/special/company/other/InsertPosition.java
@@ -0,0 +1,37 @@
+package com.abin.lee.algorithm.special.company.other;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by abin on 2018/9/3.
+ */
+public class InsertPosition {
+    public int getNum(String str) {
+        int count = 0;
+        boolean flag = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (flag == true && (i + 2) < str.length() && str.charAt(i + 2) == '0') {
+                count++;
+            }
+            if (str.charAt(i) == '1') {
+                flag = false;
+            } else if (str.charAt(i) == '0') {
+                flag = true;
+            }
+
+        }
+        return count;
+    }
+
+    /**
+     * 10001001, 在里面为0的地方插入1，保证不会出现11,1和1挨着的情况
+     * @param args
+     */
+    public static void main(String[] args) {
+        String str = "1000101";
+        int count = new InsertPosition().getNum(str);
+        System.out.println("count=" + count);
+    }
+
+}
