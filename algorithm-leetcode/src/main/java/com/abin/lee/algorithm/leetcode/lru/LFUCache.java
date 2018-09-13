fe92353a30132c9cc898c03d77fb09afb09c4349
zondahuman <zondahuman@gmail.com>
2018-09-14 00:11:14 +0800

    add

--- algorithm-leetcode/src/main/java/com/abin/lee/algorithm/leetcode/lru/LFUCache.java
+++ algorithm-leetcode/src/main/java/com/abin/lee/algorithm/leetcode/lru/LFUCache.java
@@ -1,10 +1,13 @@
 package com.abin.lee.algorithm.leetcode.lru;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by abin on 2018/9/13.
+ * 460. LFU Cache
+ * https://leetcode.com/problems/lfu-cache/description/
  */
 public class LFUCache extends HashMap<Integer, Integer> {
     private static final int defaultSize = 10;
@@ -26,11 +29,21 @@ public class LFUCache extends HashMap<Integer, Integer> {
             hitRate.hit+=1;
             hitRate.time = System.currentTimeMillis();
         }
-        return value ;
+        return value != null ? value : -1 ;
     }
 
     public void put(int key, int value) {
-
+        if(keyMap.size()>maxSize){
+            Integer key1 = getLfu();
+            super.remove(key1);
+            keyMap.remove(key1);
+        }
+        super.put(key, value);
+        keyMap.put(key, new HitRate(key, 0, System.currentTimeMillis()));
+    }
+    public Integer getLfu(){
+        HitRate min = Collections.min(keyMap.values());
+        return min.key;
     }
 
     class HitRate implements Comparable<HitRate>{
