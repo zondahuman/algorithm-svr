2326158b8ee7bfc706ff4b244457e2ab9bbe70ab
zondahuman <zondahuman@gmail.com>
2018-09-15 11:54:48 +0800

    add

--- algorithm-special/src/main/java/com/abin/lee/algorithm/special/basic/topk/TopSmallKMaxHeap.java
+++ algorithm-special/src/main/java/com/abin/lee/algorithm/special/basic/topk/TopSmallKMaxHeap.java
@@ -2,42 +2,40 @@ package com.abin.lee.algorithm.special.basic.topk;
 
 import com.abin.lee.algorithm.common.json.jackson.JsonUtil;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
  * Created by abin on 2018/9/15.
- * Top k ,求出现次数最多的, 使用小顶堆
+ * Top k ,求出现次数最少的, 使用大顶堆
  */
 public class TopSmallKMaxHeap {
     public static void main(String[] args) {
-        int[] input = new int[]{5,1,3,6,2,4};
+        int[] input = new int[]{5, 1, 3, 6, 2, 4};
         int[] param = new int[3];
-        int[] result =  new TopSmallKMaxHeap().toplargeK1(input, 3);
-        System.out.println("input="+ JsonUtil.toJson(input));
-        System.out.println("result="+ JsonUtil.toJson(result));
+        int[] result = new TopSmallKMaxHeap().topSmallK(input, 3);
+        System.out.println("input=" + JsonUtil.toJson(input));
+        System.out.println("result=" + JsonUtil.toJson(result));
     }
 
-    public void toplargeK(int[] param, int num, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(k);
-        if (queue.isEmpty() || queue.size() < k) {
-            queue.add(num);
-        }
-    }
-
-    public int[] toplargeK1(int[] param, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(k);
-        for (int i = 0; i <param.length ; i++) {
-            if(queue.size()<k) {
+    public int[] topSmallK(int[] param, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (int i = 0; i < param.length; i++) {
+            if (queue.size() < k) {
                 queue.add(param[i]);
-            }else{
-                if(param[i] > queue.peek()){
+            } else {
+                if (param[i] < queue.peek()) {
                     queue.poll();
                     queue.offer(param[i]);
                 }
             }
         }
         int[] result = new int[k];
-        for (int i = 0; i <k ; i++) {
+        for (int i = 0; i < k; i++) {
             result[i] = queue.poll();
         }
 //        int i = 0;
